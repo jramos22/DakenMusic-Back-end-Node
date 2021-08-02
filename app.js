31f93b70f83491057858ec2f465b041f8b3192c1
Kendoll Castro M <kendollcastrom.@MacBook-Pro-de-Kendoll.local>
2021-08-02 16:28:13 -0600

    Actualicé el archivo de app.js

--- app.js
+++ app.js
@@ -12,20 +12,27 @@ const app = express();
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 const PORT = process.env.PORT || 3000;
 
+/* Esto nos va a ayudar a conectar con la base de datos */
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
+
 /* .on() : Es un evento, en el cual nos va a imprimir error si algo sale mal */
 db.on('error', error=> console.log(error));
 /* .once() : Es utilizado en condiciones en las que queremos que una función en particular se ejecute solo una vez. */
 db.once('open', ()=> console.log('connection to db established'));
 
 app.use(cors());
+
+/* Nos permite obtener todo lo que venga del body como un json */
 app.use(express.json());
+
+/* Es una función de middleware incorporada en Express. Analiza las solicitudes entrantes con cargas útiles codificadas en urlencoded y se basa en body-parser.*/
 app.use(express.urlencoded({
     type:'application/x-www-form-urlencoded',
     extended: true,
 }));
 
+/* Es utilizado para llamar a las rutas, este realiza acciones y estos llaman a los controladores */
 app.use('/', userRoute);
 app.use('/', playlistRoute);
 app.use('/', favoriteRoute);
@@ -36,6 +43,7 @@ app.use('*', (req, res) =>{
     res.send("path cannot found")
 })
 
+/* .listen() : Es utilizado para vincular y escuchar las conexiones en el host y puerto especificados.*/
 app.listen(PORT, HOSTNAME, ()=>{
     console.log(`Server running on ${HOSTNAME}:${PORT}`);
 })
